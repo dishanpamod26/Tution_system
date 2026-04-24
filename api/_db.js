a56d13b10e4ac70592af2170effda5fce30b00c4
dishanpamod26 <dishanpamod26@gmail.com>
2026-04-24 14:10:56 +0530

    Fix: Improve MongoDB connection stability

--- api/_db.js
+++ api/_db.js
@@ -7,12 +7,13 @@ if (!uri) throw new Error('MONGODB_URI environment variable is not set!');
 let client = null;
 
 async function getDb() {
-    if (!client) {
+    if (!client || !client.topology || !client.topology.isConnected()) {
         client = new MongoClient(uri, {
             serverSelectionTimeoutMS: 5000,
             connectTimeoutMS: 10000,
         });
         await client.connect();
+        console.log("Connected to MongoDB");
     }
     return client.db('educore');
 }
